62050ba68140596f498e71ddbaeb927d37633bc2
Mr2000exist <sapling4740@gmail.com>
2023-11-23 12:48:32 +0900

    set img on layout - resize compatibility working

--- Caps_Buttons_proto.py
+++ Caps_Buttons_proto.py
@@ -4,16 +4,16 @@
 import math
 import sys
 #from robotics import motor_control
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QGridLayout, QHBoxLayout, QVBoxLayout, QSlider
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QGridLayout, QHBoxLayout, QVBoxLayout, QSlider, QMainWindow, QSizePolicy, QMessageBox
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor, QImage, QPixmap, QPainter
 
-resolution_w = 640 
-resolution_h = 480
+resolution_w = 480 
+resolution_h = 640
 
 button_size = math.floor(
     (resolution_w if resolution_w < resolution_h else resolution_h) / 10
 )
-
 
 class BodyControl(QWidget):
     #motor_ctrl = motor_control.MotorControl()
@@ -23,15 +23,45 @@ class BodyControl(QWidget):
         self.initUI()
 
     def initUI(self):
+
+        fileName = './grayscale_bitmap.bmp'
+
         #layout setting
         v_layout = QVBoxLayout()
         grid_layout = QGridLayout()
         kill_layout = QHBoxLayout()
         kill_layout.addStretch()
         pow_slider_layout = QVBoxLayout()
+
+        pow_slider_layout.setContentsMargins(0,0,0,0)
         
         power_slider = QSlider(Qt.Orientation.Horizontal, self)
         power_slider.setRange(1, 100)
+
+        #grid_layout.addWidget(QColor('red'))
+
+        self.imageLabel = QLabel()
+        #self.imageLabel.setBackgroundRole(QPalette.Base)
+        #self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        if fileName:
+            image = QImage(fileName)
+            if image.isNull():
+                QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
+                return
+
+            self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.scaleFactor = 1.0/(self.imageLabel.pixmap().size() * 0.003125)
+
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+            #self.imageLabel.setVisible(True)
+            #self.imageLabel.show()
+        
+        #grid_layout.cellRect(3,3)
+        #grid_layout.rowStretch(button_size*3)
+        #grid_layout.columnStretch(button_size*3)
 
         #grid_button_layout
         button_forward = QPushButton("↑", self)
@@ -60,12 +90,13 @@ class BodyControl(QWidget):
         grid_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         #power_slide_layout
-        power_slider.sliderMoved.connect(self.slider_position)
+        power_slider.valueChanged.connect(self.slider_changed)
         self.power_status_label = QLabel('1', self)
         pow_slider_layout.addWidget(power_slider)
         pow_slider_layout.addWidget(self.power_status_label)
 
         kill_layout.setContentsMargins(button_size*9, button_size, math.floor(button_size/2), 0)
+        v_layout.addWidget(self.imageLabel)
         v_layout.addLayout(kill_layout)
         v_layout.addLayout(pow_slider_layout)
         v_layout.addLayout(grid_layout)
@@ -90,7 +121,7 @@ class BodyControl(QWidget):
     def stop(self):
         self.motor_ctrl.stop()
 
-    def slider_position(self, p) : 
+    def slider_changed(self, p) : 
         self.power_status_label.setText(str(p))
 
 
