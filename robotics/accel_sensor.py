198c86afac2f1bdf428362da58f6970eb53a4a7b
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-11-29 20:23:49 +0900

    fix: Create MPU9250 class
    DESCRIPTION
    
    * This is contributed by Tae Hyeon, Jung.

--- robotics/accel_sensor.py
+++ robotics/accel_sensor.py
@@ -1,87 +1,117 @@
 import smbus
 import time
 
-MPU9250_ADDRESS = 0x68
 
-MPU9250_WHO_AM_I = 0x75
-MPU9250_ACCEL_XOUT_H = 0x3B
-MPU9250_ACCEL_YOUT_H = 0x3D
-MPU9250_ACCEL_ZOUT_H = 0x3F
-MPU9250_GYRO_XOUT_H = 0x43
-MPU9250_GYRO_YOUT_H = 0x45
-MPU9250_GYRO_ZOUT_H = 0x47
-MPU9150_RA_XOUT_L = 0x03
-MPU9150_RA_XOUT_H = 0x04
-MPU9150_RA_YOUT_L = 0x05
-MPU9150_RA_YOUT_H = 0x06
-MPU9150_RA_ZOUT_L = 0x07
-MPU9150_RA_ZOUT_H = 0x08
+class MPU9250:
+    def __init__(self, address=0x68, bus_number=1):
+        self.MPU9250_ADDRESS = address
+        self.bus = smbus.SMBus(bus_number)
 
-bus = smbus.SMBus(1)
+        self.MPU9250_WHO_AM_I = 0x75
+        self.MPU9250_ACCEL_XOUT_H = 0x3B
+        self.MPU9250_ACCEL_YOUT_H = 0x3D
+        self.MPU9250_ACCEL_ZOUT_H = 0x3F
+        self.MPU9250_GYRO_XOUT_H = 0x43
+        self.MPU9250_GYRO_YOUT_H = 0x45
+        self.MPU9250_GYRO_ZOUT_H = 0x47
+        self.MPU9150_RA_XOUT_L = 0x03
+        self.MPU9150_RA_XOUT_H = 0x04
+        self.MPU9150_RA_YOUT_L = 0x05
+        self.MPU9150_RA_YOUT_H = 0x06
+        self.MPU9150_RA_ZOUT_L = 0x07
+        self.MPU9150_RA_ZOUT_H = 0x08
 
-def read_byte(reg):
-    return bus.read_byte_data(MPU9250_ADDRESS, reg)
+        self.scale_factor_accel = 16384.0
+        self.scale_factor_gyro = 250.0 / 32768.0
+        self.scale_factor_ra = 1200.0 / 4096.0
 
-def read_word(reg):
-    high = bus.read_byte_data(MPU9250_ADDRESS, reg)
-    low = bus.read_byte_data(MPU9250_ADDRESS, reg + 1)
-    value = (high << 8) + low
-    return value
+        self.accel_data = [0, 0, 0]
+        self.gyro_data = [0, 0, 0]
+        self.ra_data = [0, 0, 0]
 
-def read_word_reverse(reg):
-    high = bus.read_byte_data(MPU9250_ADDRESS, reg)
-    low = bus.read_byte_data(MPU9250_ADDRESS, reg - 1)
-    value = (high << 8) + low
-    return value
+        who_am_i = self.read_byte(self.MPU9250_WHO_AM_I)
+        if who_am_i == 0x71:
+            print("MPU_9250 is connected.")
+        else:
+            print("MPU_9250 connection failed. WHO_AM_I =", who_am_i)
+            exit()
 
-def read_acceleration():
-    x = read_word(MPU9250_ACCEL_XOUT_H)
-    y = read_word(MPU9250_ACCEL_YOUT_H)
-    z = read_word(MPU9250_ACCEL_ZOUT_H)
-    
-    scale_factor = 16384.0
-    Axyz = [x / scale_factor, y / scale_factor, z / scale_factor,]
-    return Axyz
+    def read_byte(self, reg):
+        return self.bus.read_byte_data(self.MPU9250_ADDRESS, reg)
 
-def read_gyroscope():
-    x = read_word(MPU9250_GYRO_XOUT_H)
-    y = read_word(MPU9250_GYRO_YOUT_H)
-    z = read_word(MPU9250_GYRO_ZOUT_H)
-    
-    scale_factor = 250.0 / 32768.0
-    Gxyz = [x * scale_factor, y * scale_factor, z * scale_factor,]
-    return Gxyz
+    def read_word(self, reg):
+        high = self.bus.read_byte_data(self.MPU9250_ADDRESS, reg)
+        low = self.bus.read_byte_data(self.MPU9250_ADDRESS, reg + 1)
+        value = (high << 8) + low
+        return value
 
-def read_ra():
-    x = read_word_reverse(MPU9150_RA_XOUT_H)
-    y = read_word_reverse(MPU9150_RA_YOUT_H)
-    z = read_word_reverse(MPU9150_RA_ZOUT_H)
-    
-    scale_factor = 1200.0 / 4096.0
-    Rxyz = [x * scale_factor, y * scale_factor, z * scale_factor,]
-    return Rxyz
+    def read_word_reverse(self, reg):
+        high = self.bus.read_byte_data(self.MPU9250_ADDRESS, reg)
+        low = self.bus.read_byte_data(self.MPU9250_ADDRESS, reg - 1)
+        value = (high << 8) + low
+        return value
 
-who_am_i = read_byte(MPU9250_WHO_AM_I)
-if who_am_i == 0x71:  
-    print("MPU-9250 is connected.")
-else:
-    print("MPU-9250 connection failed. WHO_AM_I =", who_am_i)
-    exit()
+    def read_acceleration(self):
+        x = self.read_word(self.MPU9250_ACCEL_XOUT_H)
+        y = self.read_word(self.MPU9250_ACCEL_YOUT_H)
+        z = self.read_word(self.MPU9250_ACCEL_ZOUT_H)
+        self.accel_data = [
+            x / self.scale_factor_accel,
+            y / self.scale_factor_accel,
+            z / self.scale_factor_accel,
+        ]
+
+    def read_gyroscope(self):
+        x = self.read_word(self.MPU9250_GYRO_XOUT_H)
+        y = self.read_word(self.MPU9250_GYRO_YOUT_H)
+        z = self.read_word(self.MPU9250_GYRO_ZOUT_H)
+        self.gyro_data = [
+            x * self.scale_factor_gyro,
+            y * self.scale_factor_gyro,
+            z * self.scale_factor_gyro,
+        ]
+
+    def read_ra(self):
+        x = self.read_word_reverse(self.MPU9150_RA_XOUT_H)
+        y = self.read_word_reverse(self.MPU9150_RA_YOUT_H)
+        z = self.read_word_reverse(self.MPU9150_RA_ZOUT_H)
+        self.ra_data = [
+            x * self.scale_factor_ra,
+            y * self.scale_factor_ra,
+            z * self.scale_factor_ra,
+        ]
+
+    def update_data(self):
+        self.read_acceleration()
+        self.read_gyroscope()
+        self.read_ra()
+
+    def get_acceleration(self):
+        return self.accel_data
+
+    def get_gyroscope(self):
+        return self.gyro_data
+
+    def get_ra(self):
+        return self.ra_data
+
+
+if __name__ == "__main__":
+    sensor = MPU9250()
 
 try:
     while True:
-        accel_data = read_acceleration()
-        gyro_data = read_gyroscope()
-        compass_data = read_ra()
-       
+        sensor.update_data()
+        accel_data = sensor.get_acceleration()
+        gyro_data = sensor.get_gyroscope()
+        ra_data = sensor.get_ra()
+
         print("Acceleration (X,Y,Z):", accel_data)
         print("Gyroscope (X,Y,Z):", gyro_data)
-        print("Compass (X,Y,Z):", compass_data)
-       
-        time.sleep(1)
+        print("Compass (X,Y,Z):", ra_data)
 
+        time.sleep(1)
 except KeyboardInterrupt:
     pass
-
 finally:
-    bus.close()
+    sensor.bus.close()
